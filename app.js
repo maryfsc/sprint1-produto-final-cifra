de41b70dbf59d09a03eb3c5120e68824c85e729a
maryfsc <maryfsc@disroot.org>
2018-06-06 23:03:17 -0300

    Encriptação de palavra usando Cifra de César concluída. Faltando ajustes no tratamento de input.

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
 function cipher() {
-  var userInput = document.getElementById('user-input');
+  var userInput = document.getElementById('user-input').value;
 
-  if (userInput === '' || userInput === Number){
-  	alert('Digite sua palavra! Use apenas letras!');
+  if (typeof userInput === 'number' || userInput === ''){
+    alert('Digite sua palavra usando apenas letras!');
   } else {
-    
+
       var lettersArray = userInput.split('');
       var lettersCodes = []; 
-
-      for (i = 0; i < lettersArray.length; i++){ // fazendo loop nesse array para pegar os códigos ASCII
+      
+      // obtendo os códigos ASCII
+      for (i = 0; i < lettersArray.length; i++){ 
         var code = userInput.charCodeAt([i]);
         lettersCodes.push(code);
       }
 
       var cipherLettersCodes = [];
+      var cipherCodeNumber;
+      var cipherCode;
 
       // verificando se as letras são maiúsculas ou minúsculas
-      // aplicando a fórmula com o valor correspondente 
+      // aplicando a fórmula com o valor correspondente à tabela ASCII
       for (j = 0; j < lettersCodes.length; j++){ 
-      	if (lettersCodes[j] >= 65 && lettersCodes[j] <= 90){
-      	  cipherLettersCodes.push((lettersCodes[j] - 65 + 33) % 26 + 65);
-      	} else if (lettersCodes[j] >= 97 && lettersCodes[j] <= 122){
-      	  cipherLettersCodes.push((lettersCodes[j] - 97 + 33) % 26 + 97);
-      	} 
+        if (lettersCodes[j] >= 65 && lettersCodes[j] <= 90){
+          cipherCodeNumber = (lettersCodes[j] - 65 + 33) % 26 + 65;
+          cipherCode = String.fromCharCode(cipherCodeNumber);
+          cipherLettersCodes.push(cipherCode);
+        } else if (lettersCodes[j] >= 97 && lettersCodes[j] <= 122){
+          cipherCodeNumber = (lettersCodes[j] - 97 + 33) % 26 + 97;
+          cipherCode = String.fromCharCode(cipherCodeNumber);
+          cipherLettersCodes.push(cipherCode);
+        } 
       }
 
-  }
+      var encryptedMessage = cipherLettersCodes.join('');
+      var printMessage = document.getElementById('message');
+
+      return printMessage.innerHTML = 'Sua mensagem é ' + encryptedMessage + '!';
+    }
+
 }
 
 
